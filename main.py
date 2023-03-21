719e880fbf81f61b3fb558e0e5010ab006f65529
Americanfox <123677311+Americanfox@users.noreply.github.com>
2023-03-21 12:19:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-# TODO: HTTP REQUESTS
-    # Get = requests.get()
-        # We request information from the External system
-    # Post = requests.post()
-        # Send information to the external system
-    # put = requests.put()
-        # Update values
-    # delete = requests.delete()
-        # delete values
-
 import requests
 from datetime import datetime
 
@@ -23,10 +13,6 @@ user_params = {
     "notMinor": "yes",
 }
 
-# response = requests.post(url=pixela_endpopoint, json=user_params)
-
-# TODO: Advanced Authentication using an HTTP Header
-
 graph_endpoint = f"{pixela_endpopoint}/{USERNAME}/graphs"
 
 graph_config = {
@@ -36,15 +22,10 @@ graph_config = {
     "type": "float",
     "color": "momiji",
 }
-    # Headers: Like logos or numbers on a newsletter. Something that stays the same across the project.
+
 headers = {
     "X-USER-TOKEN": TOKEN,
 }
-
-# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-
-
-# TODO: ADD A PIXEL TO THE HABIT TRACKER USING A POST REQUEST
 
 pixel_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
 
@@ -56,21 +37,12 @@ pixel_config = {
     "quantity": input("How many miles did you walk today?"),
 }
 
-# pixel_response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
-# print(pixel_response.text)
-
-#TODO: How to use HTTP Put and Delete Requests
-    # HTTP Put:
 pixel_update_endpoint = f"{pixel_endpoint}/20230101"
 
 pixel_update_list = {
     "quantity": "10",
 }
 
-# response = requests.put(url=pixel_update_endpoint, json=pixel_update_list, headers=headers)
-# print(response.text)
-
-    # HTTP Delete
 pixel_delete_endpoint = f"{pixel_update_endpoint}"
 
 response = requests.delete(url=pixel_delete_endpoint, headers=headers)
